a39045d96be725ca42d7afb32348ea0b56c40457
usernamesepehr <usernamesepehr@gmail.com>
2025-11-07 11:32:51 +0330

    add: limit to get post

--- server.js
+++ server.js
@@ -6,12 +6,17 @@ const app = express();
 
 let posts = [
     { id: 1, title: "postOne"},
-    { id: 1, title: "posttow"},
-    { id: 1, title: "postThree"}
+    { id: 2, title: "posttow"},
+    { id: 3, title: "postThree"}
 ];
 
 app.get('/api/posts', (req, res) => {
-    res.json(posts);
+    const limit = parseInt(req.query.limit);
+    if(!isNaN(limit) && limit > 0) {
+        res.json(posts.slice(0,limit));
+    } else {
+        res.json(posts);
+    }
 });
 
 app.get('/api/posts/:id', (req,res) => {
