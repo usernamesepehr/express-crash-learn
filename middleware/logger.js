1d300b3739b2f471058c3feac9b34f58ff76cf3a
usernamesepehr <usernamesepehr@gmail.com>
2025-11-07 18:24:42 +0330

    add: simple usage of colors package in logger

--- middleware/logger.js
+++ middleware/logger.js
@@ -1,6 +1,20 @@
-const logger = (req,res,next) => {
-    console.log(`${req.method} ${req.protocol}://${req.get('host')}${req.originalUrl}`);
-    next();
-}
+import 'colors'; 
+
+const logger = (req, res, next) => {
+  const methodColors = {
+    GET: 'green',
+    POST: 'yellow',
+    PUT: 'blue',
+    DELETE: 'red',
+  };
+
+  const colorName = methodColors[req.method] || 'white'; // fallback
+  const logString = `${req.method} ${req.protocol}://${req.get('host')}${req.originalUrl}`;
+
+  // This is the magic line:
+  console.log(logString[colorName]);
+
+  next();
+};
 
 export default logger;
